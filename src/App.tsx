766f1b832759bc797bceaa5d9040284cf544812b
Paulo Roberto <prmorais1302@gmail.com>
2020-11-06 13:05:15 -0300

    Implementando query allPosts com graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function App() {
-  return (
-		<p>Alô React</p>
-  );
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+  cache: new InMemoryCache(),
+});
+
+interface IPost {
+  id: number;
+  title: string;
+  description: string;
 }
+
+const App = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  client
+    .query({
+      query: gql`
+        {
+          allPosts {
+            id
+            title
+            description
+          }
+        }
+      `,
+    })
+    .then((result) => setPosts(result.data.allPosts));
+
+  return (
+    <div className="container">
+      <div className="row p-5">
+        {posts.map((post) => (
+          <div className="col-md-4" key={post.id}>
+            <div className="card">
+              <div className="card-body">
+                <div className="card-title">
+                  <h4>{post.title}</h4>
+                </div>
+                <p className="card-text">{post.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
