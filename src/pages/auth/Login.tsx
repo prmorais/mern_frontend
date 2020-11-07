fd647598844f3ccdfe0f8e9589dfaaaceefe2adf
Paulo Roberto <prmorais1302@gmail.com>
2020-11-07 00:16:17 -0300

    Implementando forms de login e registro de usuarios

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 const Login: React.FC = () => {
+	const [email, setEmail] = useState<string>('');
+	const [loading] = useState<boolean>(false);
+
+	const handleSubmit = () => {
+
+	}
+
 	return (
-		<div className="container">
-			<div className="row p-5">
-				<h4>Login</h4>
-			</div>
+		<div className="container p-5">
+			<h4>Login</h4>
+			<form onSubmit={handleSubmit} >
+				<div className="form-group">
+					<label>Email</label>
+					<input
+						type="email"
+						className="form-control"
+						value={email}
+						onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+						placeholder="Entre com o e-mail"
+						disabled={loading}
+					/>
+				</div>
+				<button className="btn btn-raised btn-primary"
+					disabled={!email || loading} >
+					Submit
+							</button>
+			</form>
 		</div>
 	);
 }
 
-export default Login;
+export default Login; 
