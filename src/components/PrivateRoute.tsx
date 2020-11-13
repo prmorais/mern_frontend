ce9b84529d92dbb708788253d8dd18c6fa70863d
Paulo Roberto <prmorais1302@gmail.com>
2020-11-13 20:52:23 -0300

    Implementando Pivate Route

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,55 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, Route } from 'react-router-dom';
+import { AuthContext } from '../context/authContext';
+
+const PrivateRouter: React.FC = ({ children, ...rest }) => {
+	const { state } = useContext(AuthContext);
+	const [user, setUser] = useState(false);
+
+	useEffect(() => {
+		if (state.user) {
+			setUser(true);
+		}
+	}, [state.user]);
+
+	const navLinks = () => (
+		<nav>
+			<ul className="nav flex-column">
+				<li className="nav-item">
+					<Link to="/update/profile" className="nav-link">
+						Profile
+					</Link>
+				</li>
+
+				<li className="nav-item">
+					<Link to="/update/password" className="nav-link">
+						Password
+					</Link>
+				</li>
+
+				<li className="nav-item">
+					<Link to="/create/post" className="nav-link">
+						Post
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+
+	const renderContent = () => (
+		<div className="container-fluid pt-5">
+			<div className="row">
+				<div className="col-md-4">
+					{navLinks()}
+				</div>
+				<div className="col-md-8">
+					<Route {...rest} />
+				</div>
+			</div>
+		</div>
+	);
+
+	return user ? renderContent() : <h4>Carregando...</h4>;
+}
+
+export default PrivateRouter;
