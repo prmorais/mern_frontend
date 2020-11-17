4f733054e6c8b56c4aca9f206e96237a6df0d979
Paulo Roberto <prmorais1302@gmail.com>
2020-11-17 20:41:04 -0300

    Implementando formulário para atualizar Profile

--- src/pages/auth/Profile.tsx
+++ src/pages/auth/Profile.tsx
@@ -53,11 +53,92 @@ const Profile = () => {
 				about: data.profile.about,
 				images: data.profile.images,
 			})
-	}, [data])
+	}, [data]);
+
+	const { about, email, images, name, username } = values;
+
+	const handleSubmit = () => {
+		setLoading(false)
+	};
+
+	const handleChange = () => {
+
+	};
+
+	const handleImageChange = () => {
+
+	}
+
+	const profileUpdateForm = () => (
+		<form onSubmit={handleSubmit}>
+			<div className="form-group">
+				<label>Nome do Usuário</label>
+				<input
+					name="username"
+					value={username}
+					onChange={handleChange}
+					placeholder="Nome do usuário"
+					disabled={loading}
+					type="text"
+					className="form-control" />
+			</div>
+
+			<div className="form-group">
+				<label>Nome</label>
+				<input
+					name="name"
+					value={name}
+					onChange={handleChange}
+					placeholder="Nome"
+					disabled={loading}
+					type="text"
+					className="form-control" />
+			</div>
+
+			<div className="form-group">
+				<label>E-mail</label>
+				<input
+					name="email"
+					value={email}
+					onChange={handleChange}
+					placeholder="Email"
+					disabled={loading}
+					type="text"
+					className="form-control" />
+			</div>
+
+			<div className="form-group">
+				<label>Imagem</label>
+				<input
+					name="image"
+					// value={images}
+					onChange={handleImageChange}
+					placeholder="Imagens"
+					type="file"
+					accept="image/*"
+					className="form-control" />
+			</div>
+
+			<div className="form-group">
+				<label>Sobre</label>
+				<textarea
+					name="about"
+					value={about}
+					onChange={handleChange}
+					placeholder="Sobre"
+					disabled={loading}
+					className="form-control" />
+			</div>
+			<button
+				type="submit"
+				disabled={!email || loading}
+				className="btn btn-primary">Enviar</button>
+		</form>
+	)
 
 	return (
 		<div className="container p-5">
-			{ JSON.stringify(values)}
+			{profileUpdateForm()}
 		</div>
 	)
 }
