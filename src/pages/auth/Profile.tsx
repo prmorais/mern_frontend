c9f508226d8a722ee78ce7322c144a9ae6c3b067
Paulo Roberto <prmorais1302@gmail.com>
2020-11-17 19:11:36 -0300

    Implementando a query profile no apollo client

--- src/pages/auth/Profile.tsx
+++ src/pages/auth/Profile.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import { gql, useQuery } from '@apollo/client';
+import React, { useState } from 'react'
+
+const PROFILE = gql`
+	query {
+		profile {
+			_id
+			username
+			name
+			email
+			images {
+				url
+				public_id
+			}
+			about
+			createdAt
+			updatedAt
+		}
+	}
+`;
 
 const Profile = () => {
+	const [values, setValues] = useState({
+		username: '',
+		name: '',
+		email: '',
+		about: '',
+		images: [],
+	});
+
+	const [loading, setLoading] = useState(false);
+
+	const { data } = useQuery(PROFILE);
+
 	return (
-		<h4>
-			Perfil
-		</h4>
+		<div className="container p-5">
+			{ JSON.stringify(data)}
+		</div>
 	)
 }
 
