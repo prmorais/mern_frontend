cd0d0a6d59bdb8bc2d9f730e83e5467f37430c3f
Paulo Roberto <prmorais1302@gmail.com>
2020-11-18 10:49:07 -0300

    Usando Fragment do GraphQL

--- src/pages/auth/Profile.tsx
+++ src/pages/auth/Profile.tsx
@@ -1,201 +1,197 @@
-import { gql, useMutation, useQuery } from '@apollo/client';
-import React, { ChangeEvent, FormEvent, useMemo, useState } from 'react'
+import { gql, useMutation, useQuery } from "@apollo/client";
+import React, { ChangeEvent, FormEvent, useMemo, useState } from "react";
 
-import { toast } from 'react-toastify';
-import omitDeep from 'omit-deep-lodash';
+import { toast } from "react-toastify";
+import omitDeep from "omit-deep-lodash";
 
 interface IImage {
-	url: string,
-	public_id: string,
+  url: string;
+  public_id: string;
 }
 
 interface IProfile {
-	username: string,
-	name: string,
-	email: string,
-	about: string,
-	images: [IImage],
+  username: string;
+  name: string;
+  email: string;
+  about: string;
+  images: [IImage];
 }
 
 interface IProfileResp {
-	profile: IProfile,
+  profile: IProfile;
 }
 
+const USER_INFO = gql`
+  fragment userInfo on User {
+    _id
+    username
+    name
+    email
+    images {
+      url
+      public_id
+    }
+    about
+    createdAt
+    updatedAt
+  }
+`;
+
 const PROFILE = gql`
-	query {
-		profile {
-			_id
-			username
-			name
-			email
-			images {
-				url
-				public_id
-			}
-			about
-			createdAt
-			updatedAt
-		}
-	}
+  query {
+    profile {
+      ...userInfo
+    }
+  }
+  ${USER_INFO}
 `;
 
 const USER_PROFILE = gql`
-	mutation userUpdate($input: UserUpdateInput!) {
-		userUpdate(input: $input) {
-			_id
-			username
-			name
-			email
-			images {
-				url
-				public_id
-			}
-			about
-			createdAt
-			updatedAt
-		}
-	}
+  mutation userUpdate($input: UserUpdateInput!) {
+    userUpdate(input: $input) {
+      ...userInfo
+    }
+  }
+  ${USER_INFO}
 `;
 
 const Profile = () => {
-	const [values, setValues] = useState<IProfile>({
-		username: '',
-		name: '',
-		email: '',
-		about: '',
-		images: [{ url: '', public_id: '' }],
-	});
+  const [values, setValues] = useState<IProfile>({
+    username: "",
+    name: "",
+    email: "",
+    about: "",
+    images: [{ url: "", public_id: "" }],
+  });
 
-	const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-	const { data } = useQuery<IProfileResp>(PROFILE);
+  const { data } = useQuery<IProfileResp>(PROFILE);
 
+  useMemo(() => {
+    // Inicio da gambiarra
+    let images: any;
 
-	useMemo(() => {
-		// Inicio da gambiarra
-		let images: any;
+    if (data) {
+      // Retira o campo __typename da resposta
+      images = omitDeep(data.profile.images, "__typename");
+    }
+    // fim da gambiarra
 
-		if (data) {
-			// Retira o campo __typename da resposta
-			images = omitDeep(data.profile.images, '__typename');
-		}
-		// fim da gambiarra
+    data &&
+      setValues({
+        //...values,
+        username: data.profile.username,
+        name: data.profile.name,
+        email: data.profile.email,
+        about: data.profile.about,
+        images,
+      });
+  }, [data]);
 
-		data &&
-			setValues({
-				//...values,
-				username: data.profile.username,
-				name: data.profile.name,
-				email: data.profile.email,
-				about: data.profile.about,
-				images,
-			})
-	}, [data]);
+  // Mutation
+  const [userUpdate] = useMutation(USER_PROFILE, {
+    update: ({ data }: any) => {
+      console.log("Atualização de usuário na mutation profile", data);
 
-	// Mutation
-	const [userUpdate] = useMutation(USER_PROFILE, {
-		update: ({ data }: any) => {
-			console.log('Atualização de usuário na mutation profile', data);
+      toast.success(`Perfil do usuário <<${values.email}>> foi atualizado.`);
+    },
+  });
 
-			toast.success('Perfil do usuário foi atualizado.');
-		}
-	});
+  // Destructure
+  const { about, email, name, username } = values;
 
-	// Destructure
-	const { about, email, name, username } = values;
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(values);
 
-	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		console.log(values);
+    setLoading(true);
+    userUpdate({ variables: { input: values } });
+    setLoading(false);
+  };
 
-		setLoading(true);
-		userUpdate({ variables: { input: values } });
-		setLoading(false);
-	};
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
 
-	const handleChange = (
-		event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-	) => {
-		setValues({ ...values, [event.target.name]: event.target.value });
-	};
+  const handleImageChange = () => {};
 
-	const handleImageChange = () => {
+  const profileUpdateForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Nome do Usuário</label>
+        <input
+          name="username"
+          value={username}
+          onChange={handleChange}
+          placeholder="Nome do usuário"
+          disabled={loading}
+          type="text"
+          className="form-control"
+        />
+      </div>
 
-	}
+      <div className="form-group">
+        <label>Nome</label>
+        <input
+          name="name"
+          value={name}
+          onChange={handleChange}
+          placeholder="Nome"
+          disabled={loading}
+          type="text"
+          className="form-control"
+        />
+      </div>
 
-	const profileUpdateForm = () => (
-		<form onSubmit={handleSubmit}>
-			<div className="form-group">
-				<label>Nome do Usuário</label>
-				<input
-					name="username"
-					value={username}
-					onChange={handleChange}
-					placeholder="Nome do usuário"
-					disabled={loading}
-					type="text"
-					className="form-control" />
-			</div>
+      <div className="form-group">
+        <label>E-mail</label>
+        <input
+          name="email"
+          value={email}
+          onChange={handleChange}
+          placeholder="Email"
+          disabled
+          type="text"
+          className="form-control"
+        />
+      </div>
 
-			<div className="form-group">
-				<label>Nome</label>
-				<input
-					name="name"
-					value={name}
-					onChange={handleChange}
-					placeholder="Nome"
-					disabled={loading}
-					type="text"
-					className="form-control" />
-			</div>
+      <div className="form-group">
+        <label>Imagem</label>
+        <input
+          onChange={handleImageChange}
+          placeholder="Imagens"
+          type="file"
+          accept="image/*"
+          className="form-control"
+        />
+      </div>
 
-			<div className="form-group">
-				<label>E-mail</label>
-				<input
-					name="email"
-					value={email}
-					onChange={handleChange}
-					placeholder="Email"
-					disabled={loading}
-					type="text"
-					className="form-control" />
-			</div>
+      <div className="form-group">
+        <label>Sobre</label>
+        <textarea
+          name="about"
+          value={about}
+          onChange={handleChange}
+          placeholder="Sobre"
+          disabled={loading}
+          className="form-control"
+        />
+      </div>
+      <button
+        type="submit"
+        disabled={!email || loading}
+        className="btn btn-primary"
+      >
+        Enviar
+      </button>
+    </form>
+  );
 
-			<div className="form-group">
-				<label>Imagem</label>
-				<input
-					onChange={handleImageChange}
-					placeholder="Imagens"
-					type="file"
-					accept="image/*"
-					className="form-control" />
-			</div>
+  return <div className="container p-5">{profileUpdateForm()}</div>;
+};
 
-			<div className="form-group">
-				<label>Sobre</label>
-				<textarea
-					name="about"
-					value={about}
-					onChange={handleChange}
-					placeholder="Sobre"
-					disabled={loading}
-					className="form-control" />
-			</div>
-			<button
-				type="submit"
-				disabled={!email || loading}
-				className="btn btn-primary">Enviar</button>
-		</form>
-	)
-
-	return (
-		<div className="container p-5">
-			{profileUpdateForm()}
-		</div>
-	)
-}
-
-export default Profile
-
-
-
+export default Profile;
