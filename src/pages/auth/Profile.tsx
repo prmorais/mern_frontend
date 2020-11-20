d5849b2edd01206fdc67ee5b4a37bfe215897e10
Paulo Roberto <prmorais1302@gmail.com>
2020-11-19 21:00:16 -0300

    Fazendo upload de imagem do react para cloudinary - CORRIGIDO

--- src/pages/auth/Profile.tsx
+++ src/pages/auth/Profile.tsx
@@ -1,10 +1,10 @@
 import { useMutation, useQuery } from "@apollo/client";
 import React, {
-  ChangeEvent,
-  FormEvent,
-  useContext,
-  useMemo,
-  useState,
+	ChangeEvent,
+	FormEvent,
+	useContext,
+	useMemo,
+	useState,
 } from "react";
 
 import axios from "axios";
@@ -18,198 +18,197 @@ import { USER_UPDATE } from "../../graphql/mutations";
 import { AuthContext } from "../../context/authContext";
 
 interface IImage {
-  url: string;
-  public_id: string;
+	url: string;
+	public_id: string;
 }
 
 interface IProfile {
-  username: string;
-  name: string;
-  email: string;
-  about: string;
-  images: Array<IImage>;
+	username: string;
+	name: string;
+	email: string;
+	about: string;
+	images: Array<IImage>;
 }
 
 interface IProfileResp {
-  profile: IProfile;
+	profile: IProfile;
 }
 
 const Profile = () => {
-  const { state } = useContext(AuthContext);
-  const [values, setValues] = useState<IProfile>({
-    username: "",
-    name: "",
-    email: "",
-    about: "",
-    images: [],
-  });
+	const { state } = useContext(AuthContext);
+	const [values, setValues] = useState<IProfile>({
+		username: "",
+		name: "",
+		email: "",
+		about: "",
+		images: [{ url: '', public_id: '' }],
+	});
 
-  const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(false);
 
-  const { data } = useQuery<IProfileResp>(PROFILE);
+	const { data } = useQuery<IProfileResp>(PROFILE);
 
-  useMemo(() => {
-    // Inicio da gambiarra
-    let images: any;
+	useMemo(() => {
+		// Inicio da gambiarra
+		let images: any;
 
-    if (data) {
-      // Retira o campo __typename da resposta
-      images = omitDeep(data.profile.images, "__typename");
-    }
-    // fim da gambiarra
+		if (data) {
+			// Retira o campo __typename da resposta
+			images = omitDeep(data.profile.images, "__typename");
+		}
+		// fim da gambiarra
 
-    data &&
-      setValues({
-        //...values,
-        username: data.profile.username,
-        name: data.profile.name,
-        email: data.profile.email,
-        about: data.profile.about,
-        images,
-      });
-  }, [data]);
+		data &&
+			setValues({
+				username: data.profile.username,
+				name: data.profile.name,
+				email: data.profile.email,
+				about: data.profile.about,
+				images,
+			});
+	}, [data]);
 
-  // Mutation
-  const [userUpdate] = useMutation(USER_UPDATE, {
-    update: ({ data }: any) => {
-      console.log("Atualização de usuário na mutation profile", data);
+	// Mutation
+	const [userUpdate] = useMutation(USER_UPDATE, {
+		update: ({ data }: any) => {
+			// console.log("Atualização de usuário na mutation profile", data);
 
-      toast.success(`Perfil do usuário <<${values.email}>> foi atualizado.`);
-    },
-  });
+			toast.success(`Perfil de <<${values.email}>> atualizado.`);
+		},
+	});
 
-  // Destructure
-  const { about, email, name, username } = values;
+	// Destructure
+	const { about, email, images, name, username } = values;
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(values);
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
 
-    setLoading(true);
-    userUpdate({ variables: { input: values } });
-    setLoading(false);
-  };
+		setLoading(true);
+		userUpdate({ variables: { input: values } });
+		setLoading(false);
+	};
 
-  const handleChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    setValues({ ...values, [event.target.name]: event.target.value });
-  };
+	const handleChange = (
+		event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) => {
+		setValues({ ...values, [event.target.name]: event.target.value });
+	};
 
-  const fileResizeAndUpload = (event: ChangeEvent<any>) => {
-    let fileInput = false;
-    if (event.target.files[0]) {
-      fileInput = true;
-    }
-    if (fileInput) {
-      Resizer.imageFileResizer(
-        event.target.files[0],
-        300,
-        300,
-        "JPEG",
-        100,
-        0,
-        (uri) => {
-          // console.log(uri);
-          axios
-            .post<IImage>(
-              `${process.env.REACT_APP_REST_ENDPOINT}/uploadimage`,
-              { image: uri },
-              {
-                headers: {
-                  authorization: state.user.token,
-                },
-              }
-            )
-            .then((response) => {
-              setLoading(false);
-              console.log("Upload para Cloudinary", response);
-              setValues({ ...values, images: [response.data] });
-            })
-            .catch((err) => {
-              setLoading(false);
-              console.log("Upload para cloudinary falhou!");
-            });
-        },
-        "base64"
-      );
-    }
-  };
+	const fileResizeAndUpload = (event: ChangeEvent<any>) => {
+		let fileInput = false;
 
-  const profileUpdateForm = () => (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label>Nome do Usuário</label>
-        <input
-          name="username"
-          value={username}
-          onChange={handleChange}
-          placeholder="Nome do usuário"
-          disabled={loading}
-          type="text"
-          className="form-control"
-        />
-      </div>
+		if (event.target.files[0]) {
+			fileInput = true;
+		}
 
-      <div className="form-group">
-        <label>Nome</label>
-        <input
-          name="name"
-          value={name}
-          onChange={handleChange}
-          placeholder="Nome"
-          disabled={loading}
-          type="text"
-          className="form-control"
-        />
-      </div>
+		if (fileInput) {
+			Resizer.imageFileResizer(
+				event.target.files[0],
+				300,
+				300,
+				"JPEG",
+				100,
+				0,
+				(uri) => {
+					axios
+						.post<IImage>(
+							`${process.env.REACT_APP_REST_ENDPOINT}/uploadimage`,
+							{ image: uri },
+							{
+								headers: {
+									authorization: state.user.token,
+								},
+							}
+						)
+						.then((response) => {
+							setLoading(false);
+							// console.log("Upload para Cloudinary", response);
+							setValues({ ...values, images: [...images, response.data] });
+						})
+						.catch((err) => {
+							setLoading(false);
+							console.log("Upload para cloudinary falhou!");
+						});
+				},
+				'base64'
+			);
+		}
+	};
 
-      <div className="form-group">
-        <label>E-mail</label>
-        <input
-          name="email"
-          value={email}
-          onChange={handleChange}
-          placeholder="Email"
-          disabled
-          type="text"
-          className="form-control"
-        />
-      </div>
+	const profileUpdateForm = () => (
+		<form onSubmit={handleSubmit}>
+			<div className="form-group">
+				<label>Nome do Usuário</label>
+				<input
+					name="username"
+					value={username}
+					onChange={handleChange}
+					placeholder="Nome do usuário"
+					disabled={loading}
+					type="text"
+					className="form-control"
+				/>
+			</div>
 
-      <div className="form-group">
-        <label>Imagem</label>
-        <input
-          onChange={fileResizeAndUpload}
-          placeholder="Imagens"
-          type="file"
-          accept="image/*"
-          className="form-control"
-        />
-      </div>
+			<div className="form-group">
+				<label>Nome</label>
+				<input
+					name="name"
+					value={name}
+					onChange={handleChange}
+					placeholder="Nome"
+					disabled={loading}
+					type="text"
+					className="form-control"
+				/>
+			</div>
 
-      <div className="form-group">
-        <label>Sobre</label>
-        <textarea
-          name="about"
-          value={about}
-          onChange={handleChange}
-          placeholder="Sobre"
-          disabled={loading}
-          className="form-control"
-        />
-      </div>
-      <button
-        type="submit"
-        disabled={!email || loading}
-        className="btn btn-primary"
-      >
-        Enviar
+			<div className="form-group">
+				<label>E-mail</label>
+				<input
+					name="email"
+					value={email}
+					onChange={handleChange}
+					placeholder="Email"
+					disabled
+					type="text"
+					className="form-control"
+				/>
+			</div>
+
+			<div className="form-group">
+				<label>Imagem</label>
+				<input
+					onChange={fileResizeAndUpload}
+					placeholder="Imagens"
+					type="file"
+					accept="image/*"
+					className="form-control"
+				/>
+			</div>
+
+			<div className="form-group">
+				<label>Sobre</label>
+				<textarea
+					name="about"
+					value={about}
+					onChange={handleChange}
+					placeholder="Sobre"
+					disabled={loading}
+					className="form-control"
+				/>
+			</div>
+			<button
+				type="submit"
+				disabled={!email || loading}
+				className="btn btn-primary"
+			>
+				Enviar
       </button>
-    </form>
-  );
+		</form>
+	);
 
-  return <div className="container p-5">{profileUpdateForm()}</div>;
+	return <div className="container p-5">{profileUpdateForm()}</div>;
 };
 
 export default Profile;
